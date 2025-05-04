a45c951ee4d2702babcb6bde161293a69f7f0799
Amazing-Ant <jealon824@gmail.com>
2025-05-04 17:03:48 +0900

    player_Move_animation

--- Assets/Scripts/Entities/Field/Player/PlayerController.cs
+++ Assets/Scripts/Entities/Field/Player/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5.0f; // 기본 이동 속도
     public float runSpeed = 10.0f; // 달리기 속도
     public Transform cameraTransform; // 카메라 Transform
-    public float detectRange = 10.0f; // 탐지 범뤼
+    public float detectRange = 10.0f; // 탐지 범위
 
     private CharacterController characterController; // 캐릭터 컨트롤러
     private Animator animator; // Animator 컴포넌트
@@ -17,16 +17,16 @@ public class PlayerController : MonoBehaviour
     private GameObject currentCharacter; // 현재 활성화된 캐릭터
     private int currentCharacterIndex = 0; // 현재 활성화된 캐릭터 인덱스
 
-
     public static event Action<GameObject> OnPlayerNearEnemy; // 적 탐지 이벤트
 
     void Start()
     {
-        // 컴포넌트 가져오기
-        characterController = GetComponent<CharacterController>(); // CharacterController
+        // 캐릭터 컨트롤러 컴포넌트 가져오기
+        characterController = GetComponent<CharacterController>();
 
         // 초기 캐릭터 생성
         ChangeCharacter(currentCharacterIndex);
+
     }
 
     void Update()
@@ -36,6 +36,9 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeCharacter(0);
         if (Input.GetKeyDown(KeyCode.Alpha2)) ChangeCharacter(1);
         if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeCharacter(2);
+
+
+        #region 플레이어 이동 관련
 
         // 달리기 상태 토글
         if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetMouseButtonDown(1)) // 쉬프트 키 또는 마우스 우클릭
@@ -73,21 +76,49 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f); // 부드럽게 회전
         }
 
+        #endregion
+
+        #region 애니메이션
+
         // 애니메이션 처리
         if (animator != null)
         {
+            Vector3 anim_dir = new Vector3(horizontal, 0, vertical);
+            anim_dir = anim_dir.normalized;
+
             if (moveDirection != Vector3.zero)
             {
                 if (isRunning)
-                    animator.SetFloat("MoveMotion", 1); // 달리기
+                    animator.SetFloat("MoveMotion", anim_dir.magnitude); // 달리기
                 else if (!isRunning)
-                    animator.SetFloat("MoveMotion", 0.95f); // 걷기
+                {
+                    // 만약 MoveMotion의 값이 0.7f를 넘긴다면
+                    if (anim_dir.magnitude > 0.7f)
+                    {
+                        animator.SetFloat("MoveMotion", 0.7f);  // 수치를 고정시킨다
+                    }
+                    // 0.7f를 넘기지 않는다면
+                    else
+                    {
+                        animator.SetFloat("MoveMotion", anim_dir.magnitude);   // 걷기
+                    }
+                }
             }
             else
                 animator.SetFloat("MoveMotion", 0); // 대기
         }
+
+        else
+            Debug.Log("애니메이터가 없습니다");        
     }
 
+    #endregion
+
+
+    /// <summary>
+    /// 프리펩에 있는 플레이어 인스턴스시켜 자식오브젝트로 만듦
+    /// </summary>
+    /// <param name="newIndex"></param>
     private void ChangeCharacter(int newIndex)
     {
         if (currentCharacter != null) Destroy(currentCharacter); // 이미 활성화된 캐릭터가 있으면 파괴
@@ -97,6 +128,10 @@ public class PlayerController : MonoBehaviour
         animator = currentCharacter.GetComponent<Animator>(); // Animator 컴포넌트 가져오기
     }
 
+
+    /// <summary>
+    /// 에너미 탐지 함수
+    /// </summary>
     private void DetectEnemies()
     {
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectRange); // 탐지 범위 내의 콜라이더 가져오기
